25c9f0e81a2169755ba207bb83772d52486ab2b8
SageShi <sjshi@foxmail.com>
2019-06-11 19:09:15 +0800

    ssj xiugai

---
+++ DeepHack_source/Assets/Scripts/LadderTrigger/LadderTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name==player.name)//玩家触碰到梯子
+        {
+            print("TriggerEnter");
+            other.gameObject.GetComponent<Move>().SetIsClimb(true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == player.name)//玩家离开梯子
+        {
+            print("TriggerExit");
+            other.gameObject.GetComponent<Move>().SetIsClimb(false);
+
+        }
+    }
+}
---
+++ DeepHack_source/Assets/Scripts/Move/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 6.0F;//移动速度
+    public float gravity = 10.0F;
+    public bool isClimb = false;//是否在爬梯子
+
+    private Vector3 moveDirection = Vector3.zero;//移动方向
+    private CharacterController controller;//unity的角色控制器
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    { 
+        if (!isClimb)//走
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);//从物体坐标转化到世界坐标
+            moveDirection.y -= gravity * Time.deltaTime;//加重力
+            controller.Move(moveDirection * speed * Time.deltaTime); 
+        }
+        else//爬梯子
+        {
+            if (Input.GetKey(KeyCode.Space))//空格键爬
+            {
+                controller.Move( Vector3.up* speed * Time.deltaTime);
+            }
+
+        }      
+    }
+
+    public void SetIsClimb(bool IsClimb)
+    {
+        isClimb = IsClimb;
+    }
+}
