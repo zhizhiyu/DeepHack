6c7ba540199832f564959c5a4a31d3a2335afeee
ljm123456789 <1924245722@qq.com>
2019-06-13 11:20:47 +0800

    增加了碰撞后旋转90度、碰撞后跟随物体

---
+++ DeepHack_source/Assets/Scripts/Machines/CollisionFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/****************************************************************
+ *Copyright(C)  2019 by luojinming All rights reserved. 
+ *FileName:     CollisionFollow.cs
+ *Author:       luojinming 
+ *Version:      1.0 
+ *Date:         6.13
+ *Description:  碰撞后挂在物体上面
+ *              
+ *History:     (1):6.13,10:46
+*****************************************************************/
+
+public class CollisionFollow : MonoBehaviour
+{
+    public bool isFollow;
+    public GameObject followObject;
+
+    public int height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFollow = false;
+    }
+
+
+    private void Update()
+    {
+        if (isFollow)
+        {
+            transform.parent.position = followObject.GetComponent<Transform>().position + new Vector3(0, 1, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isFollow && collision.gameObject.Equals(followObject))
+        {
+            Debug.Log("following");
+            isFollow = true;
+        }
+    }
+}
---
+++ DeepHack_source/Assets/Scripts/Machines/CollisionRotateController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/****************************************************************
+ *Copyright(C)  2019 by luojinming All rights reserved. 
+ *FileName:     CollisionRotateController.cs
+ *Author:       luojinming 
+ *Version:      1.0 
+ *Date:         6.13
+ *Description:  机关碰撞后旋转90度
+ *              
+ *History:     (1):6.13,9:53
+*****************************************************************/
+public class CollisionRotateController : MonoBehaviour
+{
+    public bool isX;
+    public bool isY;
+    public bool isZ;
+    public bool isRotate;
+
+    public int angle;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRotate = false;
+        angle = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if(isRotate)
+        {
+            if(isX)
+            {
+                transform.Rotate(new Vector3(1, 0, 0));
+            }
+            if(isY)
+            {
+                transform.Rotate(new Vector3(0, 1, 0));
+            }
+            if(isZ)
+            {
+                transform.Rotate(new Vector3(0, 0, 1));
+            }
+            angle++;
+        }
+        if(angle >= 90)
+        {
+            isRotate = false;
+            angle = 0;
+        }
+
+    }
+
+    //碰撞后旋转
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Sphere")
+        {
+            Debug.Log("cube1 collision entered!");
+            isRotate = true;
+
+            angle = 0;
+
+        }
+    }
+
+}
