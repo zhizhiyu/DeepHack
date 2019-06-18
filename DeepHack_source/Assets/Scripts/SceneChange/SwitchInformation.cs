1dd96bff93bae0e728d7ecafebff38bf8e8780e0
ZhitaoLiang <937730534@qq.com>
2019-06-18 15:30:39 +0800

    增加情报库页面以及文档详情页面跳转逻辑

---
+++ DeepHack_source/Assets/Scripts/SceneChange/SwitchInformation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchInformation : MonoBehaviour
+{
+
+    public Text indexText;
+    public Text informationText;//记录文档文本
+
+    public void LastInformation()
+    {
+        print("last record loading");
+        int index = int.Parse(indexText.text) - 1;
+
+        //读取上一个索引的文档
+        //读取上一索引的记录，判断是否已开启
+        while (--index >= 0 && !Game.globalData.reports[index].isOpen) { } //循环直到上一条记录已打开，或已是第一条记录
+
+        if (index >= 0) //已打开的上一条记录索引大于等于0
+        {
+            //显示当前索引
+            indexText.text = (index + 1).ToString();
+            //读取记录文档
+            List<ReportConfigData> reportConfigDatas = Game.globalData.ReadReportConfigData();//读取文档列表
+            string recordTitle = reportConfigDatas[index].name;//显示文档标题
+            string recordCont = reportConfigDatas[index].description;//文档内容
+                                                                     //titleText.text = recordTitle;//标题显示到Text中
+            informationText.text = "<b><size=35>" + recordTitle + "</size></b>" + recordCont;
+        }
+    }
+
+    public void NextInformation()
+    {
+        print("next record loading");
+        int index = int.Parse(indexText.text) - 1;
+
+        //读取下一个索引的文档
+        //读取下一索引的记录，判断是否已开启
+        while (++index < Game.globalData.reports.Count && !Game.globalData.reports[index].isOpen) { } //循环直到下一条记录已打开，或已是最后一条记录
+
+        if (index < Game.globalData.reports.Count) //已打开的下一条记录索引小于等于最后一条
+        {
+            //显示当前索引
+            indexText.text = (index + 1).ToString();
+            //读取记录文档
+            List<ReportConfigData> reportConfigDatas = Game.globalData.ReadReportConfigData();//读取文档列表
+            string recordTitle = reportConfigDatas[index].name;//显示文档标题
+            string recordCont = reportConfigDatas[index].description;//文档内容
+                                                                     //titleText.text = recordTitle;//标题显示到Text中
+            informationText.text = "<b><size=35>" + recordTitle + "</size></b>" + recordCont;
+        }
+    }
+}
