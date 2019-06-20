47af2a55cade690a4ee5c2f630552f9e0fa678da
ZhitaoLiang <937730534@qq.com>
2019-06-20 15:43:57 +0800

    增加游戏中UI，暂停，继续游戏，重新开始，回到菜单等功能

---
+++ DeepHack_source/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    public void ClickPause()
+    {
+        Game.PauseGame();
+        SetActive(pausePanel, true);
+    }
+
+    public void ClickContinue()
+    {
+        SetActive(pausePanel, false);
+        Game.ContinueGame();
+    }
+
+    public void ClickRestart()
+    {
+        SetActive(pausePanel, false);
+        Game.ReStartScene();
+    }
+
+    public void ClickReturn()
+    {
+        SetActive(pausePanel, false);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+
+    static public void SetActive(GameObject go, bool state)
+    {
+        if (go == null)
+        {
+            return;
+        }
+
+        if (go.activeSelf != state)
+        {
+            go.SetActive(state);
+        }
+    }
+}
