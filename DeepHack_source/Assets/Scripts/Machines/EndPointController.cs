5d8fd85b206edcc74d3c46d0483b9bd1c716438b
ljm123456789 <1924245722@qq.com>
2019-06-21 13:55:34 +0800

    增加了通关点脚本

---
+++ DeepHack_source/Assets/Scripts/Machines/EndPointController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPointController : MonoBehaviour
+{
+    //关卡全局的object
+    public GameObject sceneControllerObject;
+
+    //玩家
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == player)
+        {
+            sceneControllerObject.GetComponent<SceneController>().Win();
+        }
+    }
+}
