487ca2cdf8db6765af96a73cda493e40e86c85b9
ZhitaoLiang <937730534@qq.com>
2019-06-21 15:31:05 +0800

    增加通关面板

---
+++ DeepHack_source/Assets/Scripts/UI/StagePass.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StagePass : MonoBehaviour
+{
+    public int currPassLevel;//当前通关号
+    public Text collectionText;//本关收集物
+    public Text timeText;//本关所用时间
+    public Text scoreText;//本关评级
+    // Start is called before the first frame update
+    void Start()
+    {
+        int currCollect = 0;
+        int sumOfCollection = 0;
+        collectionText.text = currCollect.ToString() + "/" + sumOfCollection.ToString();//显示本关收集情况
+        timeText.text = null;//显示本关通关时间
+        scoreText.text = null;//显示本关评级
+    }
+
+    //各按钮点击函数
+    
+    //下一关
+    void clickNext()
+    {
+        string nextLevel = "level_" + (currPassLevel + 1).ToString();//下一关场景名
+        SceneManager.LoadScene(nextLevel);//加载下一关
+    }
+
+    //重玩
+    void clickRestart()
+    {
+        Game.ReStartScene();//重新开始游戏
+    }
+
+    //返回菜单
+    void clickReturn()
+    {
+        SceneManager.LoadScene("MainMenu");//返回主菜单
+    }
+}
