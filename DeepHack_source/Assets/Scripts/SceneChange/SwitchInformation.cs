0b09fe81b879d64ec0b5a87724e52059633bc13c
ZhitaoLiang <937730534@qq.com>
2019-06-26 11:40:05 +0800

    文档详情界面增加翻页音效

--- DeepHack_source/Assets/Scripts/SceneChange/SwitchInformation.cs
+++ DeepHack_source/Assets/Scripts/SceneChange/SwitchInformation.cs
@@ -8,6 +8,7 @@ public class SwitchInformation : MonoBehaviour
 
     public Text indexText;
     public Text informationText;//记录文档文本
+    public AudioSource readNextAudio;//翻页声音
 
     public void LastInformation()
     {
@@ -20,6 +21,9 @@ public class SwitchInformation : MonoBehaviour
 
         if (index >= 0) //已打开的上一条记录索引大于等于0
         {
+            //翻页声音播放
+            this.readNextAudio.Play();
+
             //显示当前索引
             indexText.text = (index + 1).ToString();
             //读取记录文档
@@ -36,12 +40,17 @@ public class SwitchInformation : MonoBehaviour
         print("next record loading");
         int index = int.Parse(indexText.text) - 1;
 
+
         //读取下一个索引的文档
         //读取下一索引的记录，判断是否已开启
         while (++index < 13 && !Game.globalData.reports[index].isOpen) { } //循环直到下一条记录已打开，或已是最后一条记录
 
         if (index < 13) //已打开的下一条记录索引小于等于最后一条
         {
+
+            //翻页声音播放
+            this.readNextAudio.Play();
+
             //显示当前索引
             indexText.text = (index + 1).ToString();
             //读取记录文档
